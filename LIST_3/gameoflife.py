81de573ca420166afd184a98cf8316f3bcaced37
YahooMing <oskich2002@gmail.com>
2025-03-23 15:33:10 +0100

    Change tk to matplotlib

--- LIST_3/gameoflife.py
+++ LIST_3/gameoflife.py
@@ -1,40 +1,26 @@
 import random
-#import matplotlib as mp
-import tkinter as tk
+import matplotlib.pyplot as plt
 
 class GameOfLife:
-    def __init__(self,base, mode=1):
+    def __init__(self, mode=1):
         self.width = 100
         self.height = 100
         self.mode = mode
 
-        # TEMP
-        self.size_cell = 10
-        self.background = tk.Canvas(base, width=self.width * self.size_cell, height=self.height * self.size_cell)
-        self.background.pack()
-        self.base = base
-
-
-        self.table = [ [0 for _ in range(self.width)] for _ in range(self.height)]
+        self.table = [[0 for _ in range(self.width)] for _ in range(self.height)]
         self.random_table()
 
-        self.draw() #temp
-        self.play()
+        self.fig, self.ax = plt.subplots()
+        self.im = self.ax.imshow(self.table, cmap="binary", interpolation="nearest")
+        self.fig.canvas.mpl_connect('close_event', self.on_close)
+
+        self.running = True
 
     def random_table(self):
         for row in range(self.height):
             for col in range(self.width):
                 self.table[row][col] = random.choice([0, 1])
-    def draw(self):
-        for i in range(self.height):
-            for j in range(self.width):
-                clr = "white" if self.table[i][j] == 1 else "black"
-                self.background.create_rectangle(
-                    j * self.size_cell, i * self.size_cell,
-                    (j + 1) * self.size_cell, (i + 1) * self.size_cell,
-                    fill=clr
-                )
-    
+
     def colide(self, x, y):
         shifts = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
         living_cells = 0
@@ -42,11 +28,9 @@ class GameOfLife:
             close_cell_x, close_cell_y = (x + dx) % self.height, (y + dy) % self.width
             living_cells += self.table[close_cell_x][close_cell_y]
         return living_cells
-    
+
     def update(self):
         new_table = [[0 for _ in range(self.width)] for _ in range(self.height)]
-        changed_cells = []  # Lista zmienionych komórek
-
         for row in range(self.height):
             for col in range(self.width):
                 mystate = self.table[row][col]
@@ -55,23 +39,20 @@ class GameOfLife:
                     new_table[row][col] = 0
                 elif mystate == 0 and neighbours == 3:  # Urodziny
                     new_table[row][col] = 1
-                else:  
+                else:
                     new_table[row][col] = mystate  # Stan się nie zmienia
-                
-                # Śledzenie zmian
-                if new_table[row][col] != self.table[row][col]:
-                    changed_cells.append((row, col, new_table[row][col]))
-
         self.table = new_table
-        return changed_cells  # Zwracamy listę zmienionych komórek
-    
+
     def play(self):
-        self.update()
-        self.background.delete("all")
-        self.draw()
-        self.base.after(10, lambda: self.play())
+        while self.running:
+            self.update()
+            inverted_table = [[1 - cell for cell in row] for row in self.table]
+            self.im.set_data(inverted_table)
+            plt.pause(0.1)
+
+    def on_close(self, event):
+        self.running = False
 
 if __name__ == "__main__":
-    base = tk.Tk()
-    game = GameOfLife(base)
-    base.mainloop()
+    game = GameOfLife()
+    game.play()
