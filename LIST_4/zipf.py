9ce606a173831c4554900fe4d2d573e4758709d3
YahooMing <oskich2002@gmail.com>
2025-03-28 18:24:28 +0100

    List 4 done?

--- LIST_4/zipf.py
+++ LIST_4/zipf.py
@@ -3,21 +3,43 @@ from collections import defaultdict
 import matplotlib.pyplot as plt
 
 def zipf(file):
-    dict = defaultdict(int)
-    with open(file, 'r') as txt:
+    words = defaultdict(int)
+    with open(file, 'r', encoding='utf-8') as txt:
         lines = txt.readlines()
         file_content = ''.join(lines)
         clean_string = re.sub('[^A-Za-z0-9]+', ' ', file_content)
         clean_string = clean_string.lower()
         #print(clean_string)
         for word in clean_string.split():
-            if word not in dict:
-                dict[word] = 1
+            if word not in words:
+                words[word] = 1
             else:
-                dict[word] +=  1
+                words[word] +=  1
         #print(dict)
 
-    sorted_dict = sorted(dict.items(), key = lambda item: item[i], reverse = True)
+    sorted_dict = dict(
+        sorted(
+            words.items(),
+            key=lambda x: x[1],
+            reverse=True)
+        )
+    rank = []
+    frequency = []
+    x = 0
+
+    for freq in sorted_dict.values():
+        x += 1
+        rank.append(x)
+        frequency.append(freq)
+
+    plt.plot(rank, frequency)
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.xlabel("Ranga")
+    plt.ylabel("Częstotliwość")
+    plt.title("Prawo Zipfa")
+    plt.show()
+
 
 
 
